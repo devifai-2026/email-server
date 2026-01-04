1f1e01f42c64d59a1f87ba5233e23962fabe82dc
subhojit dutta <dsubhojit063@gmail.com>
2026-01-04 18:16:05 +0530

    t

--- src/routes/paypal.route.js
+++ src/routes/paypal.route.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const {
   createOrder,
   captureOrder,
+  stripeWebhook,
 } = require("../controllers/paypal.controller");
 const { protect } = require("../middlewares/auth.middleware");
 
 // POST /api/paypal/create-order
+router.post(
+  '/stripe-webhook',
+  express.raw({ type: 'application/json' }),
+  stripeWebhook
+);
 router.post("/create-order", protect, createOrder);
 
 router.post("/capture-order", protect, captureOrder);
